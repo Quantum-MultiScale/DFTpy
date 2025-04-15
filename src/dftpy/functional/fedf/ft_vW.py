96373e9cefe7e2680ed69291ce66f8ba3a886c83
Macheng <macheng97@126.com>
2025-04-15 15:38:05 +0800

    ft_vW.py

--- src/dftpy/functional/fedf/ft_vW.py
+++ src/dftpy/functional/fedf/ft_vW.py
@@ -6,16 +6,43 @@ from dftpy.functional.functional_output import FunctionalOutput
 from dftpy.math_utils import PowerInt
 from dftpy.time_data import timer
 from dftpy.field import DirectField
-from dftpy.functional.fedf import FTK,FTK_dt,get_reduce_t
-from dftpy.constants import Units
+from dftpy.functional.fedf import FTH,FTH_dt,get_reduce_t
+
 __all__ = ['FT_vW','FT_vWStress']
+
+def get_vW_kedensity(rho):
+    """
+    positive define vW kinetic energy densiy  
+    | \nabla \rho |^2 / (8 \rho) 
+    """
+    rhoGrad = rho.gradient()
+    eden = np.sqrt(PowerInt(rhoGrad[0], 2) + PowerInt(rhoGrad[1], 2) + PowerInt(rhoGrad[2], 2)) / 8.0 / rho
+    return eden
+
+def vW_GQ(rho,h):
+    """
+    the vW_GQ function moved from ATLAS 
+    """
+    sqrt_rho = PowerInt(rho, 1, 2)
+    gradient_sqrt_rho = sqrt_rho.gradient()
+    hgs_rho  = gradient_sqrt_rho*h
+    grho_x   = ( hgs_rho[0].gradient(ipol=0) 
+               + hgs_rho[1].gradient(ipol=1) 
+               + hgs_rho[2].gradient(ipol=2) )
+    pot = -0.5 * grho_x/sqrt_rho 
 
 def FT_vWPotential(rho,FT_T):
     """
     Finite Temperature vW Potential
+    
+    tau -> ground state vW energy density 
     """
-
-    pot *=  
+    t = get_reduce_T(rho,FT_T)
+    h = FTH(t)
+    h_dt = FTH_dt(t) 
+    tau = get_vW_kedensity(rho) 
+    pot =  vW_GQ(rho,h)
+    pot = pot + (-2.0/3.0) * tau * h_dt * t / rho
 
     return pot 
 
@@ -23,7 +50,11 @@ def FT_vWEnergy(rho,FT_T):
     """
     Finite Temperature vW Energy
     """
-
+    t = get_reduce_T(rho,FT_T)
+    h = FTH(t) 
+    eden = get_vW_kedensity(rho) * h 
+    eden = eden * rho.grid.dV 
+    ene = eden.sum() 
     return ene
 
 def FT_vWStress(rho,x=1.0,temperature=1e-3,**kwargs):
