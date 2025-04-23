a987ebe135debf91db96d9ec53ab4acd521515cd
Lan, Jian <shapeless.cat@gmail.com>
2025-04-23 19:22:38 +0800

    Add tests/ and example test cases
    - pytest config has been added to pyproject.toml

---
+++ tests/unit/utils/test_utils.py
@@ -0,0 +1,18 @@
+import pytest
+
+from dftpy.utils.utils import *
+
+
+@pytest.mark.parametrize(
+    "rho, n, expected",
+    [
+        (0, 1, 0),
+        (1, 2, 2),
+        (1, 3, 3),
+    ]
+)
+def test_calc_rho(rho, n, expected):
+    assert calc_rho(rho, N=n) == expected
+
+def test_calc_drho():
+    assert calc_drho(1, 3) == pytest.approx(6.0)
