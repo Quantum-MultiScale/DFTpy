90224143262644cd51b59ec1fda4f2ef53246f30
Macheng <macheng97@126.com>
2025-04-28 22:13:37 +0800

    bugs

--- src/dftpy/functional/fedf/ft_wt.py
+++ src/dftpy/functional/fedf/ft_wt.py
@@ -1,4 +1,5 @@
 # Collection of finite temperature Thomas Fermi functional
+from IPython.terminal.shortcuts.auto_suggest import accept_and_keep_cursor
 
 from dftpy.field import DirectField
 from dftpy.functional.functional_output import FunctionalOutput
@@ -8,7 +9,7 @@ __all__ = ['FT_WT', 'FT_WT']
 
 def FT_WTPotential(rho, kernel, alpha=5.0 / 6.0, beta=5.0 / 6.0):
     """
-    Finite Temperature WT Potential
+    Finite Temperature WT Potentia
     """
     if (alpha - beta) > 1e-10:
         raise RuntimeError("alpha neq beta, FT_WTPotential not work!")
@@ -45,7 +46,8 @@ def FT_WTStress(rho, x=1.0, temperature=1e-3, **kwargs):
 def FT_WT(rho, calcType={"E", "V"}, temperature=1e-3, **kwargs):
     """
     temperature in eV 
-    FT_T in Ha 
+    FT_T in Ha
+    aaauu
     """
     # HARTREE2EV = Units.Ha
     # has changed in hartree
@@ -58,3 +60,14 @@ def FT_WT(rho, calcType={"E", "V"}, temperature=1e-3, **kwargs):
     if "V" in calcType:
         OutFunctional.potential = FT_WTPotential(rho, FT_T)
     return OutFunctional
+
+
+def get_WT_kernel_table(kernel_table: dict, rho0: float, temperature: float,
+                        max_eta: float, neta: int, delta_eta: float):
+    kernel_table['max_eta'] = max_eta
+    kernel_table['neta'] = neta
+    kernel_table['delta_eta'] = delta_eta
+    kernel_table['rho0'] = rho0
+    kernel_table['temperature'] = temperature
+
+    return True
