cc8c43ea0f2ca7068652c649403ef5209e27d193
Macheng <macheng97@126.com>
2025-04-29 15:18:50 +0800

    wt bf test

--- src/dftpy/functional/fedf/ft_wt.py
+++ src/dftpy/functional/fedf/ft_wt.py
@@ -39,12 +39,15 @@ def FT_WTStress(rho, x=1.0, temperature=1e-3, **kwargs):
     """
     Finite Temperature WT Stress
     """
+    stress = np.zeros((3, 3))
+    stress_ii = 0.0
     for i in range(3):
         stress[i, i] = stress_ii
     return stress
 
 
-def FT_WT(rho, calcType={"E", "V"}, temperature=1e-3, **kwargs):
+def FT_WT(rho, ke_kernel_saved, calcType={"E", "V"}, temperature=1e-3,
+          **kwargs):
     """
     temperature in eV 
     FT_T in Ha
@@ -54,17 +57,24 @@ def FT_WT(rho, calcType={"E", "V"}, temperature=1e-3, **kwargs):
     # print( "temperature",temperature)
     FT_T = temperature
     OutFunctional = FunctionalOutput(name="FT_TF")
+    rho0 = np.mean(rho)
+    neta = 120000
+    max_eta = 50.0
+    delta_eta = max_eta / neta
+    kernel = _fill_kernel(rho, ke_kernel_saved, rho0, temperature,
+                          max_eta=max_eta,
+                          neta=neta, delta_eta=delta_eta)
     if "E" in calcType:
-        ene = FT_WTEnergy(rho, FT_T)
+        ene = FT_WTEnergy(rho, kernel)
         OutFunctional.energy = ene
     if "V" in calcType:
-        OutFunctional.potential = FT_WTPotential(rho, FT_T)
+        OutFunctional.potential = FT_WTPotential(rho, kernel)
     return OutFunctional
 
 
 def get_WT_kernel_table(kernel_table: dict, rho0: float, temperature: float,
                         max_eta: float, neta: int, delta_eta: float,
-                        alpha=5.0 / 6.0, beta=5.0 / 6.0) -> bool:
+                        maxp=100000, alpha=5.0 / 6.0, beta=5.0 / 6.0) -> bool:
     if check_kernel_table(kernel_table, rho0, temperature): return True
     init_kernel_table(kernel_table, max_eta, neta, delta_eta)
     kf = (3.0 * np.pi ** 2 * rho0) ** (1.0 / 3.0)
@@ -73,6 +83,41 @@ def get_WT_kernel_table(kernel_table: dict, rho0: float, temperature: float,
     chem_pot = get_chemical_potential(rho0, temperature)
     chi_tf = fermi__1_2_elegent(chem_pot / temperature)
     chi_tf = 0.5 * (2.0 * temperature) ** (0.5) * chi_tf / kf
-    kernel_table['eat'] = np.zeros(neta)
+    kernel_table['eta'] = np.zeros(neta)
+    kernel_table['weta'] = np.zeros(neta)
+    for ii in range(0, neta):
+        eta = ii * delta_eta
+        kernel_table['eta'][ii] = eta
+        chi_vw = 4.0 / 3.0 / (2.0 * eta) ** 2
+        chi_lr = ft_lindhard(eta, rho0, temperature, maxp=maxp)
+        kernel_table['weta'][ii] = 1.0 / chi_lr - 1.0 / chi_vw - 1.0 / chi_tf
+
+    kernel_table['weta'] = coef * kernel_table['weta']
 
     return True
+
+
+def _fill_kernel(rho, ke_kernel_saved: dict, rho0: float, temperature: float,
+                 max_eta: float, neta: int, delta_eta: float,
+                 maxp=100000, alpha=5.0 / 6.0, beta=5.0 / 6.0):
+    if ke_kernel_saved['kernel_table'] is None:
+        ke_kernel_saved['kernel_table'] = {}
+
+    kernel_table_update = get_WT_kernel_table(ke_kernel_saved['kernel_table'],
+                                              rho0, temperature,
+                                              max_eta, neta, delta_eta,
+                                              maxp=maxp, alpha=alpha,
+                                              beta=beta)
+    if kernel_table_update or (ke_kernel_saved['kernel'] is None):
+        tkf = 2.0 * (3.0 * np.pi ** 2 * rho0) ** (1.0 / 3.0)
+        gg = rho.grid.gg
+        kernel = np.zeros_like(gg)
+        kernel_flat = kernel.flatten()
+        kernel_flat = np.interp(kernel_flat,
+                                ke_kernel_saved['kernel_table']['eta'],
+                                ke_kernel_saved['kernel_table']['weta'])
+        kernel = kernel_flat.reshape(kernel.shape)
+        ke_kernel_saved['kernel'] = kernel
+    else:
+        kernel = ke_kernel_saved['kernel']
+    return kernel
